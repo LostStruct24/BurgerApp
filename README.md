e153f545c74f9383acf29e197c1e25dab5a073bf
N <75640078+N12345678987654321@users.noreply.github.com>
2020-12-31 15:11:23 +0400

    BurgerApp

---
+++ README.md
@@ -0,0 +1,5 @@
+Burger Builder app written with React JS.
+
+Getting started:
+Fork or directly clone this repository to your local machine and cd into the project directory.
+Run npm install and then npm start.
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import BurgerLogo from '../../assets/images/logo.png';
+import './Logo.css';
+
+const Logo = (props) => {
+  return (
+    <div className="Logo"
+      style={{ height: props.height, marginBottom: props.marginBottom }}>
+      <img src={BurgerLogo} alt="MyBurger" />
+    </div>
+  );
+};
+
+
+export default Logo;
